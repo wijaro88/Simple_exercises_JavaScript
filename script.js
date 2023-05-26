60a166466b801dff1586616c873b77d319e5d8b3
Wilson R <wijaro88@gmail.com>
2023-05-25 19:00:34 -0500

    1linea

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Este es un escript para aprender
+:)
+*/
+
+// alert("hola mundo 2.0");
+
+console.log("Hola desde la consola");
+
+document.write("<h2>Hola desde la pagina</h2>");
+
+var nombre = prompt("Escribe tu nombre: ");
+
+// document.write(nombre);
+document.write("<h2> ¡Bienvenido " + nombre + "! :)</h2>");
+
+console.log(typeof nombre);
+
+let edad=37;
+console.log(typeof edad);
+
+let ejemploBoolean = true;
+console.log(typeof ejemploBoolean);
+
+document.write("<h2>mi edad es " + edad + "</>");
